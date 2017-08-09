308b79e1549973cdd041679868077e446ba393bc
Daniel <dktdaniel@gmail.com>
2017-08-08 23:10:45 -0700

    Added comments

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,29 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-let getElementsByClassName = (function(className) {
-	function DOMtraverser(node, className) {
-		let classMatches = [];
-     	// check if the node has any classes at all and if it does, if its class is a match
-     	if (node.classList && node.classList.contains(className)) {
-       	//child's class is a match, add it to classMatches array
-       	classMatches.push(node);
-     	}
-     	// check if the node has child nodes
-     	let children = node.childNodes;
-     	//if it does...
-     	if (typeof children !== undefined) {       
-       		//check each child if they match the target class
-       		//we can't use forEach because a node list is not an array
-       		for (let i = 0; i < children.length; i++) {
-         	// send each child back through the DOMtraverser
-         	// the classMatches array from this call is generated within a new scope so we need to concat it to the original
-         	classMatches = classMatches.concat(DOMtraverser(children[i], className));
-       		}
-     	}
-     	return classMatches;				
+let getElementsByClassName = function(className) {
+  function DOMtraverser(node) {
+    let classMatches = [];
+      // check if the node has any classes at all and if it does, if its class is a match
+      if (node.classList && node.classList.contains(className)) {
+        //child's class is a match, add it to classMatches array
+        classMatches.push(node);
+      }
+      // check if the node has child nodes
+      let children = node.childNodes;
+      //if it does...
+      if (typeof children !== undefined) {       
+          //check each child if they match the target class
+          //we can't use forEach because a node list is not an array
+          for (let i = 0; i < children.length; i++) {
+          // send each child back through the DOMtraverser
+          // the classMatches array from this call is generated within a new scope so we need to concat it to the original
+          classMatches = classMatches.concat(DOMtraverser(children[i]));
+          }
+      }
+      return classMatches;        
    }
- 	return function(className) {
-    	return DOMtraverser(document.body, className);
-  	};     
-})();
+  //call DOMtraverser passing in document.body as the starting node
+  return DOMtraverser(document.body);     
+};
